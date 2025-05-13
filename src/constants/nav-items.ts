ab18fa454a7aa3017206620f8787236c0e36d575
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-13 13:32:58 -0300

    add navigation items and create Home page with Hero component

---
+++ src/constants/nav-items.ts
@@ -0,0 +1,8 @@
+export const NAV_BAR_ITEMS = {
+  Inicio: {
+    link: "/",
+  },
+  Ingressos: {
+    link: "#buy",
+  },
+};
---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "../components/hero";
+
+export const Home = () => {
+  return (
+    <div className="w-full flex flex-col">
+      <Hero />
+    </div>
+  );
+};
