f9e36bbf5a73b7cb38b851e6eb2b4f53a0330654
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-14 14:34:52 -0300

    add Know component with header and card mapping for event information

---
+++ src/components/know/index.tsx
@@ -0,0 +1,27 @@
+import { KNOW_CARDS } from "../../constants/knowConstants";
+import { Container } from "../container";
+import { KnowCard } from "./know-card";
+
+export const Know = () => {
+  return (
+    <Container invert>
+      <h1 className="text-white text-2xl font-bold mb-10 text-center">
+        Saiba mais sobre tudo que acontecerá <br /> na imersão de 2 dias de
+        evento
+      </h1>
+
+      <div className="flex flex-row gap-4 flex-wrap items-center justify-center">
+        {KNOW_CARDS.map((card) => (
+          <KnowCard key={card.title} {...card} />
+        ))}
+      </div>
+
+      <p className="text-xl text-white mt-10 max-w-[700px] text-center mx-auto z-20">
+        Estes seis comportamentos não são apenas estratégias, mas sim pilares de
+        uma filosofia que eleva tanto o líder quanto sua empresa. Eles são as
+        pedras angulares no arco da conquista, sustentando uma estrutura que se
+        eleva em direção ao sucesso empreendedor.
+      </p>
+    </Container>
+  );
+};
