03c90f62e0a832b28a029b24ed4587880054aaf6
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-14 14:35:11 -0300

    add Know and Speakers components to Home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,13 @@
 import { Hero } from "../components/hero";
+import { Know } from "../components/know";
+import { Speakers } from "../components/speakers";
 
 export const Home = () => {
   return (
     <div className="w-full flex flex-col">
       <Hero />
+      <Know />
+      <Speakers />
     </div>
   );
 };
