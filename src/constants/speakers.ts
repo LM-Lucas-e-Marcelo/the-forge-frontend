1272d61ae1420453d6c5649efa62c9712580ef16
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-15 13:20:34 -0300

    add SPEAKERS constant with speaker details and images

---
+++ src/constants/speakers.ts
@@ -0,0 +1,36 @@
+import rafael from "../assets/speakers/palestrante.png";
+import lucas from "../assets/speakers/lucas.png";
+import paulo from "../assets/speakers/paulo.png";
+
+export const SPEAKERS = [
+  {
+    name: "Rafael Winterhalter",
+    position: "Criador do Byte Buddy",
+    imgUrl: rafael,
+  },
+  {
+    name: "Lucas Oliveira",
+    position: "Desenvolvedor Backend na Zup",
+    imgUrl: lucas,
+  },
+  {
+    name: "Paulo Silveira",
+    position: "CEO na Alura",
+    imgUrl: paulo,
+  },
+  {
+    name: "Lucas Oliveira",
+    position: "Desenvolvedor Backend na Zup",
+    imgUrl: lucas,
+  },
+  {
+    name: "Paulo Silveira",
+    position: "CEO na Alura",
+    imgUrl: paulo,
+  },
+  {
+    name: "Rafael Winterhalter",
+    position: "Criador do Byte Buddy",
+    imgUrl: rafael,
+  },
+];
