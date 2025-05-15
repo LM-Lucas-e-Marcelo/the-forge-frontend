231f9280e4f866cef91eb60a861ffebb57aeaa9e
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-15 13:20:27 -0300

    add speaker list with names and images to Speakers component

--- src/components/speakers/index.tsx
+++ src/components/speakers/index.tsx
@@ -1,9 +1,24 @@
+import { SPEAKERS } from "../../constants/speakers";
 import { Container } from "../container";
+import { SpeakerCard } from "./speaker-card";
 
 export const Speakers = () => {
   return (
     <Container>
-      <div className="h-[600px]" />
+      <span className="text-6xl font-bold text-white flex flex-col gap-3 text-center">
+        <h1 className="mr-40">Palestrantes</h1>
+        <h1 className="ml-40">Confirmados</h1>
+      </span>
+      <div className="flex flex-wrap gap-4 mt-20 justify-center">
+        {SPEAKERS.map((speaker) => (
+          <SpeakerCard
+            key={speaker.name}
+            name={speaker.name}
+            position={speaker.position}
+            imgUrl={speaker.imgUrl}
+          />
+        ))}
+      </div>
     </Container>
   );
 };
