23f12332c2cb204c764f786dd56ab63331ffe50d
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-16 15:29:17 -0300

    add call-to-action button in Header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,10 @@
 import logo from "../assets/logo.png";
 import { NAV_BAR_ITEMS } from "../constants/nav-items";
+import { Button } from "./form/button";
 
 export const Header = () => {
   return (
-    <header className="bg-black px-4 fixed top-0 left-0 right-0 z-60">
+    <header className="bg-black px-4 py-2 fixed top-0 left-0 right-0 z-60">
       <div className="w-full flex h-[70px] items-center gap-[100px] max-w-[1440px] mx-auto px-[16px]">
         <a href="/">
           <img src={logo} alt="Logo" width={150} />
@@ -17,7 +18,9 @@ export const Header = () => {
             ))}
           </ul>
         </nav>
-        <div className="flex ml-auto gap-4"></div>
+        <div className="flex ml-auto gap-4">
+          <Button>Quero ser forjado</Button>
+        </div>
       </div>
     </header>
   );
