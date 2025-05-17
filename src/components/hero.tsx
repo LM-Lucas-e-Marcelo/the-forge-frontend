9a1ec1b472a3603644d016b7759f29e7d675fdb3
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-17 12:58:54 -0300

    update Hero component to replace video background with image and adjust text positioning

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,15 +1,15 @@
-import heroMp4 from "../assets/hero.mp4";
 import waves from "../assets/waves.png";
 
 export const Hero = () => {
   return (
     <div className="relative">
-      <video muted loop autoPlay>
+      {/* <video muted loop autoPlay>
         <source src={heroMp4} type="video/mp4" />
-      </video>
+      </video> */}
+      <div className="w-full h-[700px] bg-[url(/the-forge.jpg)] bg-cover bg-center" />
       <img src={waves} className="mt-[-100px] w-full object-cover" />
-      <div className="absolute bottom-[40%] left-[24px]">
-        <h1 className="text-white text-5xl mb-5">A Forja</h1>
+      <div className="absolute bottom-[55%] left-[24px]">
+        <h1 className="text-white text-5xl mb-5 font-tertiary">A Forja</h1>
         <h2 className="text-white text-3xl max-w-[500px]">
           O MAIOR EVENTO DE GESTÃO EMPRESARIAL E LIDERANÇA DE SANTA CATARINA!
         </h2>
