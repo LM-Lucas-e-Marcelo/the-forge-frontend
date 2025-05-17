67273ff5cbd523eabc19901cfc885fd907f0f648
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-17 13:00:29 -0300

    add route for TicketsPage to enable ticket options navigation

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router";
 import { Home } from "../pages/home";
 import { DefaultLayout } from "../layouts/default-layout";
+import { TicketsPage } from "../pages/tickets-page";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<DefaultLayout />}>
         <Route index element={<Home />} />
+        <Route path="tickets" element={<TicketsPage />} />
       </Route>
     </Routes>
   );
