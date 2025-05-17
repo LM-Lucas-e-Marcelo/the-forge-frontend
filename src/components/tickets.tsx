a1265ed4b277be9d3fcd4bd1936cb1e02d28abe8
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-17 12:59:16 -0300

    add navigation functionality to Tickets component

--- src/components/tickets.tsx
+++ src/components/tickets.tsx
@@ -1,11 +1,21 @@
+import { useNavigate } from "react-router";
+
 export const Tickets = () => {
+  const naviate = useNavigate();
+
+  const handleNavigate = () => {
+    naviate("/tickets");
+  };
   return (
-    <div className="fixed bottom-2 right-2 w-[500px] flex gap-5 justify-between p-4 rounded-2xl backdrop-blur bg-zinc-500/40 z-50">
+    <div className="fixed bottom-2 left-[50%] transform -translate-x-1/2 w-[500px] flex gap-5 justify-between p-4 rounded-2xl backdrop-blur bg-zinc-500/40 z-50">
       <section className="text-white">
         <p className="text-2xl">R$ 1.890,00</p>
         <p>LOTE 1 DISPONÍVEL</p>
       </section>
-      <button className="p-4 text-white bg-gradient-to-l from-black to-primary rounded-md font-bold w-[200px]">
+      <button
+        onClick={handleNavigate}
+        className="p-4 text-white bg-gradient-to-l from-black to-primary rounded-md font-bold w-[200px]"
+      >
         Comprar Ingresso
       </button>
     </div>
