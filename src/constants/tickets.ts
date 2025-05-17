12d941aa5ad1ebad64667d0a87e1388c2bc931c0
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-17 13:00:14 -0300

    add TICKETS constant with pricing and benefits for event packages

---
+++ src/constants/tickets.ts
@@ -0,0 +1,54 @@
+export const TICKETS = [
+  {
+    title: "A FORJA",
+    benefits: [
+      "Acesso a todas as palestra na plenária principal.(2 Dias)",
+      "Acesso a Feira de Negócios.(2 Dias)",
+    ],
+    price: "98,00",
+    rate: "9,80",
+    times: 4,
+  },
+  {
+    title: "A FORJA GOLD",
+    benefits: [
+      "Acesso a todas as palestra na plenária principal.(2 Dias)",
+      "Acesso a Feira de Negócios.(2 Dias)",
+      "Lugar reservado mais próximo ao palco.",
+    ],
+    price: "248,00",
+    rate: null,
+    times: 12,
+  },
+  {
+    title: "A FORJA VIP",
+    benefits: [
+      "Acesso a todas as palestra na plenária principal.(2 Dias)",
+      "Acesso a Feira de Negócios.(2 Dias)",
+      "Lugar reservado mais próximo ao palco.",
+      "Credenciamento antecipado.",
+      "Kit de boas vindas!",
+    ],
+    price: "310,00",
+    rate: null,
+    times: 12,
+  },
+  {
+    title: "A FORJA PRIVILEGE",
+    benefits: [
+      "Acesso a todas as palestra na plenária principal.(2 Dias)",
+      "Acesso a Feira de Negócios.(2 Dias)",
+      "Lugar reservado mais próximo ao palco.",
+      "Credenciamento antecipado.",
+      "Kit de boas vindas!",
+      "Assentos especiais(Sofa/Poltronas)",
+      "Sala VIP descanso Privilege.",
+      "Open Bar (Água, Refrigerantes e Energético.)",
+      "CHOPP Liberado no Happy Hour",
+    ],
+    price: "1.199,00",
+    rate: null,
+    times: 12,
+    hasTag: true,
+  },
+];
