55615786b4d954b4ba2b34f8f2c94f599f3a468d
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-17 12:59:53 -0300

    update Speakers component to apply inverted container style and change font style for headings

--- src/components/speakers/index.tsx
+++ src/components/speakers/index.tsx
@@ -4,10 +4,13 @@ import { SpeakerCard } from "./speaker-card";
 
 export const Speakers = () => {
   return (
-    <Container>
-      <span className="text-6xl font-bold text-white flex flex-col gap-3 text-center">
-        <h1 className="mr-40">Palestrantes</h1>
-        <h1 className="ml-40">Confirmados</h1>
+    <Container invert>
+      <span
+        id="speakers"
+        className="text-6xl font-bold text-white flex flex-col gap-3 text-center"
+      >
+        <h1 className="mr-40 font-tertiary">Palestrantes</h1>
+        <h1 className="ml-40 font-tertiary">Confirmados</h1>
       </span>
       <div className="flex flex-wrap gap-4 mt-20 justify-center">
         {SPEAKERS.map((speaker) => (
