6c5f59583782e05be096fc128ab5df4be1131ed9
Lucas Sagas <lucas.sagas@rdstation.com>
2025-06-19 11:51:05 -0300

    update Tickets component: change ticket price, update navigation to external link, and adjust button styling

--- src/components/tickets.tsx
+++ src/components/tickets.tsx
@@ -1,23 +1,23 @@
-import { useNavigate } from "react-router";
-
 export const Tickets = () => {
-  const naviate = useNavigate();
-
   const handleNavigate = () => {
-    naviate("/tickets");
+    window.open(
+      "https://www.sympla.com.br/evento/a-forja-2025/2983812",
+      "_self"
+    );
   };
   return (
     <div className="fixed bottom-2 left-[50%] transform -translate-x-1/2 w-[500px] flex gap-5 justify-between p-4 rounded-2xl backdrop-blur bg-zinc-500/40 z-50">
       <section className="text-white">
-        <p className="text-2xl">R$ 1.890,00</p>
+        <p className="text-2xl">R$ 129,00</p>
         <p>LOTE 1 DISPONÍVEL</p>
       </section>
-      <button
+      <div
+        role="button"
         onClick={handleNavigate}
-        className="p-4 text-white bg-gradient-to-l from-black to-primary rounded-md font-bold w-[200px]"
+        className="p-4 text-white rounded-md font-bold w-[270px] bg-[url(/hero_bg.png)] bg-cover bg-center flex items-center justify-center cursor-pointer"
       >
-        Comprar Ingresso
-      </button>
+        PRÉ-VENDA DISPONÍVEL
+      </div>
     </div>
   );
 };
