2beeea48c9f8dffa989ace805554a47c6227258f
Lucas Sagas <lucas.sagas@rdstation.com>
2025-06-19 11:51:39 -0300

    update Home component: replace HowIKnow with TicketsInformation for enhanced ticket display

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,23 +2,21 @@ import { Hero } from "../components/hero";
 import { HowIKnow } from "../components/how-i-know";
 import { Know } from "../components/know";
 import { Location } from "../components/location";
-import { Space } from "../components/space";
 import { Speakers } from "../components/speakers";
 import { Testimonials } from "../components/testimonials";
 import { Tickets } from "../components/tickets";
-import { YouReady } from "../components/you-ready";
+import { TicketsInformation } from "../components/tickets-information";
 
 export const Home = () => {
   return (
     <div className="w-full flex flex-col">
       <Hero />
       <Know />
-      <HowIKnow />
+      <TicketsInformation />
       <Speakers />
-      <Space />
       <Testimonials />
       <Location />
-      <YouReady />
+      <HowIKnow />
       <Tickets />
     </div>
   );
