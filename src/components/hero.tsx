9c1d8fd7b5237ddc265ec7c65168e6ed54ce0c63
Lucas Sagas <lucas.sagas@rdstation.com>
2025-06-19 11:50:52 -0300

    update Hero component with new background and logo, adjust text positioning

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,18 +1,19 @@
-import waves from "../assets/waves.png";
+import logo from "../assets/logo.png";
 
 export const Hero = () => {
   return (
     <div className="relative">
-      {/* <video muted loop autoPlay>
-        <source src={heroMp4} type="video/mp4" />
-      </video> */}
-      <div className="w-full h-[700px] bg-[url(/the-forge.jpg)] bg-cover bg-center" />
-      <img src={waves} className="mt-[-100px] w-full object-cover" />
-      <div className="absolute bottom-[55%] left-[24px]">
-        <h1 className="text-white text-5xl mb-5 font-tertiary">A Forja</h1>
-        <h2 className="text-white text-3xl max-w-[500px]">
-          O MAIOR EVENTO DE GESTÃO EMPRESARIAL E LIDERANÇA DE SANTA CATARINA!
+      <div className="w-full h-[700px] bg-[url(/hero_bg.png)] bg-cover bg-center" />
+      <div className="absolute bottom-[40%] left-[30px]">
+        <img src={logo} width={300} alt="logo" />
+        <h2 className="text-white text-5xl max-w-[600px]">
+          VAI TRANSFORMAR VOCÊ!
         </h2>
+      </div>
+      <div className="absolute bottom-10 left-10 text-white">
+        <p>Grande</p>
+        <p>Florianópolis</p>
+        <p>A Forja - 2025</p>
       </div>
     </div>
   );
