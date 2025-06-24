a549158d19084beeca0952142ca8dffebad3d60a
Lucas Sagas <lucas.sagas@rdstation.com>
2025-06-24 10:31:19 -0300

    add images to tickets information component: include 3D visuals for enhanced user experience

--- src/components/tickets-information/index.tsx
+++ src/components/tickets-information/index.tsx
@@ -1,6 +1,8 @@
 import { TICKETS } from "../../constants/tickets";
 import { Container } from "../container";
 import { TicketCard } from "./ticket-card";
+import seats from "../../assets/seats.png";
+import location from "../../assets/location-3d.png";
 
 export const TicketsInformation = () => {
   return (
@@ -10,6 +12,10 @@ export const TicketsInformation = () => {
         {TICKETS.map(({ color, ...rest }, index) => (
           <TicketCard key={index} color={color as never} {...rest} />
         ))}
+      </div>
+      <div className="flex flex-col gap-4 w-full items-center justify-center mt-10">
+        <img src={location} className="w-[80%]" alt="imagem 3d do palco" />
+        <img src={seats} className="w-[80%]" alt="imagem 3d do palco" />
       </div>
     </Container>
   );
