f3613d7153c466a6e8ffee9a7ff599d6690b16b7
Lucas Sagas <lucas.sagas@rdstation.com>
2025-06-24 10:41:59 -0300

    update hero component: replace static background with YouTube video background and enhance layout

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,18 +2,26 @@ import logo from "../assets/logo.png";
 
 export const Hero = () => {
   return (
-    <div className="relative">
-      <div className="w-full h-[700px] bg-[url(/hero_bg.png)] bg-cover bg-center" />
-      <div className="absolute bottom-[40%] left-[30px]">
-        <img src={logo} width={300} alt="logo" />
-        <h2 className="text-white text-5xl max-w-[600px]">
+    <div className="relative w-[177.78vh] h-screen md:w-screen md:h-[56.25vw] overflow-hidden">
+      <iframe
+        className="absolute top-0 left-0 w-[177.78vh] h-screen md:w-screen md:h-[56.25vw] object-cover pointer-events-none"
+        src="https://www.youtube.com/embed/UWiwM-Ohp5I?autoplay=1&mute=1&controls=0&loop=1&playlist=UWiwM-Ohp5I&modestbranding=1&showinfo=0"
+        title="YouTube video background"
+        allow="autoplay; fullscreen"
+        allowFullScreen
+      ></iframe>
+
+      <div className="absolute top-0 left-0 w-[177.78vh] h-screen md:w-screen md:h-[56.25vw] bg-gradient-to-r from-black/70 via-black/50 to-black/80" />
+
+      <div className="relative z-10 flex flex-col items-start justify-center h-full px-8">
+        <img src={logo} alt="Logo" className="w-[250px]" />
+        <h1 className="text-white text-5xl max-w-[600px]">
           VAI TRANSFORMAR VOCÊ!
-        </h2>
-      </div>
-      <div className="absolute bottom-10 left-10 text-white">
-        <p>Grande</p>
-        <p>Florianópolis</p>
-        <p>A Forja - 2025</p>
+        </h1>
+        <div className="text-white absolute bottom-10">
+          <p>Grande Florianópolis</p>
+          <p>A Forja - 2025</p>
+        </div>
       </div>
     </div>
   );
