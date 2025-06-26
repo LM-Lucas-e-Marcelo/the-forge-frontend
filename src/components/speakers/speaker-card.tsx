f7ff669fc51cbadb3d69cddd2402ab004910b574
Lucas Sagas <lucas.sagas@rdstation.com>
2025-06-26 12:50:36 -0300

    update speaker card component: enhance layout with logo and improve styling for better presentation

--- src/components/speakers/speaker-card.tsx
+++ src/components/speakers/speaker-card.tsx
@@ -1,3 +1,5 @@
+import whiteLogo from "../../assets/white_logo.png";
+
 type SpeakerCardProps = {
   name: string;
   position: string;
@@ -6,14 +8,20 @@ type SpeakerCardProps = {
 
 export const SpeakerCard = ({ name, position, imgUrl }: SpeakerCardProps) => {
   return (
-    <div className="max-w-[400px] pl-2 bg-gradient-to-b from-black to-primary rounded-2xl">
-      <div className="bg-gradient-to-b from-white to-zinc-100 relative rounded-2xl overflow-hidden max-h-[400px]">
-        <img src={imgUrl} alt="Nome do palestrante" width={400} />
-        <span className="absolute bottom-0 left-0 right-0 text-center text-white z-20 p-2">
-          <h2 className="text-3xl">{name}</h2>
-          <p className="text-lg">{position}</p>
-        </span>
-        <div className="bg-gradient-to-b from-transparent to-primary w-full h-[150px] absolute bottom-0 z-10" />
+    <div className="flex text-white">
+      <img src={imgUrl} width={300} alt="speaker image" />
+      <div className="bg-gradient-to-t from-black from-[-30%] via-[#e84c2b] to-[#e84c2b] p-10 flex flex-col items-start justify-end gap-5 w-full max-w-[800px] pr-50 relative">
+        <strong className="font-tertiary text-xl">{name}</strong>
+        <div
+          className="text-md"
+          dangerouslySetInnerHTML={{ __html: position }}
+        />
+        <img
+          className="absolute top-[-10px] right-4"
+          src={whiteLogo}
+          width={100}
+          alt="logo"
+        />
       </div>
     </div>
   );
