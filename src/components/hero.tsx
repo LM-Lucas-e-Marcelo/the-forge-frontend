ac2d18c93ba9f87ec50120d455b4f861b274e4d9
Lucas Sagas <lucas.sagas@rdstation.com>
2025-06-29 16:50:29 -0300

    update Hero component: adjust logo size and heading style for improved responsiveness

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,7 @@ export const Hero = () => {
   return (
     <div className="relative w-[177.78vh] h-screen md:w-screen md:h-[56.25vw] overflow-hidden">
       <iframe
-        className="absolute top-0 left-0 w-[177.78vh] h-screen md:w-screen md:h-[56.25vw] object-cover pointer-events-none"
+        className="absolute top-0 left-0 w-[177.78vh]  h-screen md:w-screen md:h-[56.25vw] object-cover pointer-events-none sm:"
         src="https://www.youtube.com/embed/UWiwM-Ohp5I?autoplay=1&mute=1&controls=0&loop=1&playlist=UWiwM-Ohp5I&modestbranding=1&showinfo=0"
         title="YouTube video background"
         allow="autoplay; fullscreen"
@@ -14,8 +14,8 @@ export const Hero = () => {
       <div className="absolute top-0 left-0 w-[177.78vh] h-screen md:w-screen md:h-[56.25vw] bg-gradient-to-r from-black/70 via-black/50 to-black/80" />
 
       <div className="relative z-10 flex flex-col items-start justify-center h-full px-8">
-        <img src={logo} alt="Logo" className="w-[250px]" />
-        <h1 className="text-white text-5xl max-w-[600px]">
+        <img src={logo} alt="Logo" className="sm:w-[250px] w-[200px]" />
+        <h1 className="text-white sm:text-5xl text-3xl max-w-[600px]">
           VAI TRANSFORMAR VOCÊ!
         </h1>
         <div className="text-white absolute bottom-10">
