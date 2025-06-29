69b633075a8cd80fd4f1cdc5e99664276625cb5f
Lucas Sagas <lucas.sagas@rdstation.com>
2025-06-29 16:50:50 -0300

    update Know component: enhance heading size for better responsiveness and adjust card layout

--- src/components/know/index.tsx
+++ src/components/know/index.tsx
@@ -6,19 +6,19 @@ export const Know = () => {
   return (
     <div className="w-full bg-black px-10 py-30 bg-[url(/stage.webp)] bg-cover bg-center">
       <div className="max-w-[1440px] mx-auto w-full">
-        <h1 className="text-white text-5xl my-10 text-center font-tertiary">
+        <h1 className="text-white sm:text-5xl text-3xl my-10 text-center font-tertiary">
           TUDO O QUE VOCÊ PRECISA <br /> PARA IR MAIS LONGE
         </h1>
 
         <div className="flex flex-row gap-4 flex-wrap items-center justify-center mt-20">
           {KNOW_CARDS.map((card, index) => {
             return (
-              <>
+              <div className="flex flex-col sm:flex-row items-center">
                 <KnowCard key={card.title} {...card} />
                 {index !== KNOW_CARDS.length - 1 && (
-                  <div className="h-[190px] w-[3px] bg-primary rounded-full" />
+                  <div className="sm:h-[190px] sm:w-[3px] bg-primary rounded-full w-[90%] h-[3px]" />
                 )}
-              </>
+              </div>
             );
           })}
         </div>
