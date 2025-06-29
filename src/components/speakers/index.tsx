4e42f807fdcacfe32bc8ef13915350068f66c0d0
Lucas Sagas <lucas.sagas@rdstation.com>
2025-06-29 16:59:53 -0300

    update Speakers component: adjust responsive text sizes and margins for improved layout

--- src/components/speakers/index.tsx
+++ src/components/speakers/index.tsx
@@ -7,10 +7,10 @@ export const Speakers = () => {
     <Container>
       <span
         id="speakers"
-        className="text-6xl font-bold text-white flex flex-col gap-3 text-center"
+        className="sm:text-6xl text-3xl font-bold text-white flex flex-col gap-3 text-center"
       >
-        <h1 className="mr-40 font-tertiary">Palestrantes</h1>
-        <h1 className="ml-40 font-tertiary">Confirmados</h1>
+        <h1 className="sm:mr-40 mr-30 font-tertiary">Palestrantes</h1>
+        <h1 className="sm:ml-40 ml-30 font-tertiary">Confirmados</h1>
       </span>
       <div className="flex flex-col gap-6 mt-20 justify-center items-center">
         {SPEAKERS.map((speaker) => (
