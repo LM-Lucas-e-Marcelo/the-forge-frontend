d75bee7ec2c445208d27f537656c8b78453a9765
Lucas Sagas <lucas.sagas@rdstation.com>
2025-06-29 16:50:54 -0300

    update SpeakerCard component: enhance layout for better responsiveness and adjust image sizing

--- src/components/speakers/speaker-card.tsx
+++ src/components/speakers/speaker-card.tsx
@@ -8,12 +8,12 @@ type SpeakerCardProps = {
 
 export const SpeakerCard = ({ name, position, imgUrl }: SpeakerCardProps) => {
   return (
-    <div className="flex text-white">
-      <img src={imgUrl} width={300} alt="speaker image" />
-      <div className="bg-gradient-to-t from-black from-[-30%] via-[#e84c2b] to-[#e84c2b] p-10 flex flex-col items-start justify-end gap-5 w-full max-w-[800px] pr-50 relative">
+    <div className="flex text-white flex-col sm:flex-row">
+      <img src={imgUrl} className="sm:w-[300px] w-full" alt="speaker image" />
+      <div className="bg-gradient-to-t from-black from-[-30%] via-[#e84c2b] to-[#e84c2b] p-10 flex flex-col items-start justify-end gap-5 w-full max-w-[800px] sm:pr-50 relative">
         <strong className="font-tertiary text-xl">{name}</strong>
         <div
-          className="text-md"
+          className="sm:text-md text-lg"
           dangerouslySetInnerHTML={{ __html: position }}
         />
         <img
