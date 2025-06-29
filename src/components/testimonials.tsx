1853d1f1c1fa446d1cbf4c9de4554cb7f634c1d9
Lucas Sagas <lucas.sagas@rdstation.com>
2025-06-29 16:59:48 -0300

    update Testimonials component: adjust iframe dimensions for improved layout

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -11,7 +11,7 @@ export const Testimonials = () => {
         <div className="flex gap-10 mt-10">
           <div className="p-2 bg-gradient-to-b from-black to-primary rounded-2xl">
             <iframe
-              className="rounded-lg sm:w-[700px] sm:h-[394px] w-[500px] h-[270px]"
+              className="rounded-lg sm:w-[700px] sm:h-[394px] w-[350px] h-[200px]"
               src="https://www.youtube.com/embed/6aOKS3vJ6wU?si=lyi8qIIkbvAWI4rp"
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
