a9bf4075f1502f857e0d616dab43369c8ea3468f
Lucas Sagas <lucas.sagas@rdstation.com>
2025-06-30 19:13:34 -0300

    wip

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -5,42 +5,65 @@ import ga from "../assets/sponsors/ga.png";
 import meira from "../assets/sponsors/meira.png";
 import movimento from "../assets/sponsors/movimento.png";
 import prefeitura from "../assets/sponsors/prefeitura.png";
-import somar from "../assets/sponsors/somar.png";
+import sonmar from "../assets/sponsors/sonmar.png";
 import splenger from "../assets/sponsors/splenger.png";
+import mastermind from "../assets/sponsors/mastermind.png";
 
 export const Sponsors = () => {
   return (
-    <div className="flex gap-4 flex-wrap items-center justify-center p-10">
-      <img
-        src={edasuaconta}
-        alt="E da Sua Conta"
-        className="h-[60px] w-auto object-contain"
-      />
-      <img src={alves} alt="Alves" className="h-[40px] w-auto object-contain" />
-      <img
-        src={cardelik}
-        alt="Cardelik"
-        className="h-[40px] w-auto object-contain"
-      />
-
-      <img
-        src={movimento}
-        alt="Movimento"
-        className="h-[40px] w-auto object-contain"
-      />
-      <img src={meira} alt="Meira" className="h-[60px] w-auto object-contain" />
+    <div className="flex flex-col gap-12 flex-wrap items-center justify-center p-10">
       <img
         src={prefeitura}
         alt="Prefeitura"
         className="h-[40px] w-auto object-contain"
       />
-      <img src={somar} alt="Somar" className="h-[40px] w-auto object-contain" />
-      <img
-        src={splenger}
-        alt="Splenger"
-        className="h-[40px] w-auto object-contain"
-      />
-      <img src={ga} alt="GA" className="h-[60px] w-auto object-contain" />
+      <div className="flex gap-12 items-center justify-center flex-wrap">
+        <img
+          src={edasuaconta}
+          alt="E da Sua Conta"
+          className="h-[60px] w-auto object-contain"
+        />
+        <img
+          src={alves}
+          alt="Alves"
+          className="h-[40px] w-auto object-contain"
+        />
+        <img
+          src={cardelik}
+          alt="Cardelik"
+          className="h-[40px] w-auto object-contain"
+        />
+      </div>
+      <div className="flex gap-12 items-center justify-center flex-wrap">
+        <img
+          src={movimento}
+          alt="Movimento"
+          className="h-[40px] w-auto object-contain"
+        />
+        <img
+          src={meira}
+          alt="Meira"
+          className="h-[60px] w-auto object-contain"
+        />
+        <img
+          src={sonmar}
+          alt="sonmar"
+          className="h-[40px] w-auto object-contain"
+        />
+      </div>
+      <div className="flex gap-12 items-center justify-center flex-wrap">
+        <img
+          src={splenger}
+          alt="Splenger"
+          className="h-[40px] w-auto object-contain"
+        />
+        <img src={ga} alt="GA" className="h-[60px] w-auto object-contain" />
+        <img
+          src={mastermind}
+          alt="Mastermind"
+          className="h-[60px] w-auto object-contain"
+        />
+      </div>
     </div>
   );
 };
--- src/components/tickets.tsx
+++ src/components/tickets.tsx
@@ -17,15 +17,15 @@ export const Tickets = () => {
     <>
       <div className="sm:fixed bottom-2 left-[50%] transform -translate-x-1/2 w-[500px] hidden sm:flex gap-5 justify-between p-4 rounded-2xl backdrop-blur bg-zinc-500/40 z-50">
         <section className="text-white">
-          <p className="text-2xl">R$ 129,00</p>
-          <p>LOTE 1 DISPONÍVEL</p>
+          <p className="text-2xl">R$ 159,90</p>
+          <p>LOTE 1</p>
         </section>
         <div
           role="button"
           onClick={handleNavigate}
           className="p-4 text-white rounded-md font-bold w-[270px] bg-[url(/hero_bg.png)] bg-cover bg-center flex items-center justify-center cursor-pointer"
         >
-          PRÉ-VENDA DISPONÍVEL
+          VENDA DISPONÍVEL
         </div>
       </div>
       <FinishModal isOpen={openModal} onClose={handleToggleModal} />
