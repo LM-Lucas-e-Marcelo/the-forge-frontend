19c23b8e925d403a3463aba188d1ee23c8c11bc0
Lucas Sagas <lucas.sagas@rdstation.com>
2025-07-01 11:46:42 -0300

    update KNOW_CARDS constants: modify titles and subtitles for event details

--- src/constants/know-constants.ts
+++ src/constants/know-constants.ts
@@ -1,17 +1,17 @@
 export const KNOW_CARDS = [
   {
-    title: "Comece no mercado <br /> digital do jeito certo",
+    title: "Realidade do evento",
     subTitle: "10 trilhas de conteúdo",
     text: "escolhidas com base na experiência da Hotmart com mais de 200 mil creators.",
   },
   {
     title: "Aprenda com os <br/> maiores do mercado",
-    subTitle: "140 Palestrantes",
+    subTitle: "+10 Palestrantes",
     text: "com conteúdos e métodos validados por milhares de profissionais do mercado.",
   },
   {
     title: "Conecte-se e amplie <br/> sua rede de negócios",
-    subTitle: "10 mil Participantes",
-    text: "do Brasil e do mundo, reunidos em 3 dias de muito networking e aprendizado.",
+    subTitle: "2 mil Participantes",
+    text: "de Santa Catarina do Brasil, reunidos em um dia de muito networking e aprendizado.",
   },
 ];
