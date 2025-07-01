9d0133d35a31b99e85b6eb36822bc11b5280a5bd
Lucas Sagas <lucas.sagas@rdstation.com>
2025-07-01 11:46:51 -0300

    update Hero component styles: adjust logo size and heading text for improved visibility

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -13,9 +13,9 @@ export const Hero = () => {
 
       <div className="absolute top-0 left-0 w-[177.78vh] h-screen md:w-screen md:h-[56.25vw] bg-gradient-to-r from-black/70 via-black/50 to-black/80" />
 
-      <div className="relative z-10 flex flex-col items-start justify-center h-full px-8">
-        <img src={logo} alt="Logo" className="sm:w-[250px] w-[200px]" />
-        <h1 className="text-white sm:text-5xl text-3xl max-w-[600px]">
+      <div className="relative z-10 flex flex-col items-start justify-center h-full px-8 sm:ml-20 ml-0">
+        <img src={logo} alt="Logo" className="sm:w-[350px] w-[200px]" />
+        <h1 className="text-white sm:text-7xl text-3xl max-w-[600px] text-nowrap">
           VAI TRANSFORMAR VOCÊ!
         </h1>
         <div className="text-white absolute bottom-10">
