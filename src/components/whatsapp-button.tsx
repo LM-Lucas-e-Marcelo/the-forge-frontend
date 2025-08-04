262b470f6734ec533670aab0c5230d3f82cfba48
Lucas Sagás <lucassaagas@gmail.com>
2025-08-04 11:57:52 -0300

    Revert "change tel"
    This reverts commit d05f156a50ec3032f127a9770a7bfe4dcf898111.

--- src/components/whatsapp-button.tsx
+++ src/components/whatsapp-button.tsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export const WhatsappButton = () => {
   return (
     <a
-      href="https://wa.me/5548988173536"
+      href="https://wa.me/5511974421689"
       target="_blank"
       className="fixed bottom-6 right-6 z-50 flex items-center justify-center w-16 h-16 bg-green-500 rounded-full shadow-lg hover:scale-105 transition-transform"
     >
